89eff6c9339d0740236d7b2be77a29870f5cf0d3
Ivan Storck <ivan@ivanstorck.com>
2021-04-04 11:11:02 -0700

    feat(api): return weatherConditions
    default to miles per hour for windspeed

--- pages/api/csv.ts
+++ pages/api/csv.ts
@@ -1,11 +1,11 @@
 import got from 'got';
 import parse from 'csv-parse/lib/sync';
-
+import { metersPerSecondToMph} from '../util/convert';
 /**
  * @param {{ query: { station: string; }; }} req
  * @param {{ json: (arg0: { statusCode: number; body: any; }) => void; }} res
  */
-export async function handler(req, res) {
+export default async function handler(req, res) {
   const station = req.query.station || 'WPOW1'
    try {
     // @ts-ignore
@@ -27,9 +27,18 @@ export async function handler(req, res) {
     })
     console.log(records)
 
+    const weatherData = records[0]
+
+    const weatherConditions = {
+      stationId: weatherData.station_id,
+      windSpeed: metersPerSecondToMph(parseInt(weatherData['wind_speed (m/s)'])),
+      windDirection: parseInt(weatherData['wind_from_direction (degree)']),
+      windGust: metersPerSecondToMph(parseInt(weatherData['wind_speed_of_gust (m/s)']))
+    }
+
     res.json({
       statusCode: 200,
-      body: records[0]
+      weatherConditions
     })
   
   } catch (error) {
---
+++ pages/util/convert.ts
@@ -0,0 +1,3 @@
+export function metersPerSecondToMph(ms: number): number {
+  return ms * 2.23694
+}
