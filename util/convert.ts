9d37c72a6758025889b246ae542b72028a2a2790
Ivan Storck <ivan@ivanstorck.com>
2021-10-24 12:07:03 -0700

    fix function types

--- util/convert.ts
+++ util/convert.ts
@@ -2,6 +2,6 @@
  * @param {number} ms - meters per second
  * @return {number} miles per hour
  */
-export default function metersPerSecondToMph(ms) {
+export default function metersPerSecondToMph(ms: number): number {
   return ms * 2.23694
 }
