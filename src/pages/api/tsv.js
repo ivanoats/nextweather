35c9beb0e67d904674dbc665d92a1c326444183e
Ivan Storck <ivan@ivanstorck.com>
2022-01-23 11:11:18 -0800

    chore: remove stupid comment

--- src/pages/api/tsv.js
+++ src/pages/api/tsv.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const obs =  async (req, res) => {
    try {
-    // @ts-ignore
     const observations = await axios.get(`https://www.ndbc.noaa.gov/data/hourly2/hour_00.txt`)
 
     res.json({
