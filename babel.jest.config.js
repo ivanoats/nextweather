345ff587c92e4c7819f6e9a68a3ee4b268eb2b36
Ivan Storck <ivan@ivanstorck.com>
2022-01-23 10:59:35 -0800

    chore: tweak jest config

--- babel.jest.config.js
+++ babel.jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
     ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-typescript',
   ],
+  "env": {
+    "test": {
+      "plugins": [
+        "@babel/plugin-transform-modules-commonjs"
+      ]
+    }
+  }
 };
